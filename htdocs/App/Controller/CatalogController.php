93d38db98c6c6a44a8e00133dfc31be48590b8b6
Mathys Pomier <pomier.mathys@outlook.fr>
2021-06-18 13:27:01 +0200

    remove unused className attribut

--- htdocs/App/Controller/CatalogController.php
+++ htdocs/App/Controller/CatalogController.php
@@ -13,12 +13,12 @@ class CatalogController extends AbstractController
     public function view(){
         $products =  (new ProductRepository())->findAll();
         //$products = (new ProductRepository())->findBy("name", "Chemise");
-        return $this->render("catalog/view.phtml", ['products' => $products]);
+        return $this->render("catalog/view.phtml", ['pageName' => 'Catalogue', 'products' => $products]);
     }
 
     public function viewProduct(){
         $product = (new ProductRepository())->find(1);
-        return $this->render("catalog/viewProduct.phtml", ["product" => $product]);
+        return $this->render("catalog/viewProduct.phtml", ['pageName' => 'Info produit', "product" => $product]);
     }
 
 }
